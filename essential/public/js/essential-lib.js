db9d6ffaea5ce04153c308df841465a3497a30b5
= <jo123.garcia.s@gmail.com>
2018-10-24 18:49:07 -0500

    starting with menu interaction and redux is added

---
+++ essential/public/js/essential-lib.js
@@ -0,0 +1,64 @@
+/* --- REDUX --- */
+const state = {
+  menuDisplacement: true,
+};
+
+const reducer = (state, action) => {
+  const newState = Object.assign({}, state);
+  switch (action.type) {
+    case 'MENU_ON':
+      newState.menuDisplacement = false;
+      return newState;
+    case 'MENU_OFF':
+      newState.menuDisplacement = true;
+      return newState;
+    default:
+      return state;
+  }
+}
+
+const store = Redux.createStore(reducer, state);
+
+const actionMenuOn = {
+  type: 'MENU_ON',
+}
+
+const actionMenuOff = {
+  type: 'MENU_OFF',
+}
+
+const menuOn = () => {
+  store.dispatch(actionMenuOn);
+}
+
+const menuOff = () => {
+  store.dispatch(actionMenuOff);
+}
+
+const menuState = () => {
+  return store.getState().menuDisplacement;
+}
+
+store.subscribe(menuState);
+
+/* --- END REDUX --- */
+/* --- MENU INTERACTION --- */
+
+const menuButton = document.querySelector('.menu-js');
+const menuContainer = document.querySelector('.menu-container-js');
+
+const menuInteraction = () => {
+  if (menuState()) {
+    menuOn();
+    console.log(menuState());
+    menuContainer.classList.add('menu-on');
+  } else {
+    menuOff();
+    console.log(menuState());
+    menuContainer.classList.remove('menu-on');
+  }
+};
+
+menuButton.addEventListener('click', (e) => {
+  menuInteraction();
+});
